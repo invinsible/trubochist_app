4c5a9805a1eedc5183f94ded27bdc6aeec235d7f
Igor Sergeev <.sergeev@teachbase.ru>
2022-03-08 03:17:05 +0300

    Start

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="device">
+      <h3>Выберите отопительный прибор</h3>
+      <div class="grid">
+        <p>
+        <label for="kamin">Камин</label>
+        <input type="radio" id="kamin" value="kamin" v-model="device" />
+      </p>
+
+      <p>
+        <label for="pech">Печь</label>
+        <input type="radio" id="pech" value="pech" v-model="device" />
+      </p>
+
+      <p>
+        <label for="bana_pech">Банная печь</label>
+        <input type="radio" id="bana_pech" value="bana_pech" v-model="device" />
+      </p>
+
+      <p>
+        <label for="kotel">Котел</label>
+        <input type="radio" id="kotel" value="kotel" v-model="device" />
+      </p>
+      </div>
+      
+    </div>
+    <div v-show="step > 1" class="fuel">
+      <h3>Выберите топливо</h3>
+      <div class="grid">
+      <p>
+        <label for="drova">Дрова, брикеты, пеллеты</label>
+        <input type="radio" id="drova" value="drova" v-model="fuel" />
+      </p>
+      <p v-if="device === 'kamin' || device === 'kotel'">
+        <label for="gaz">Газ</label>
+        <input type="radio" id="gaz" value="gaz" v-model="fuel" />
+      </p>
+      <p v-if="device === 'pech' || device === 'kotel'">
+        <label for="ugol">Древесный уголь</label>
+        <input type="radio" id="ugol" value="ugol" v-model="fuel" />
+      </p>
+      <p v-if="device === 'kotel'">
+        <label for="dizel">Дизельное топливо</label>
+        <input type="radio" id="dizel" value="dizel" v-model="fuel" />
+      </p>
+      </div>
+      
+    </div>
+    <div v-if="step > 2" class="material">
+      <h3>Выберите материалы дымохода</h3>
+      <Material-step @choose="compareResult" :show="this.fuel"/>
+    </div>
+    <div v-if="step > 3" class="configuration">
+      <h3>Выберите конфигурацию дымохода</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MaterialStep from './components/MaterialStep.vue';
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MaterialStep,
+  },
+  data() {
+    return {
+      step: 1,
+      device: null,
+      fuel: null,
+      
+      result: {
+        device: null,
+        fuel: null,
+        material: null
+      },
+    };
+  },
+  watch: {    
+    device() {
+      this.step = 2;
+    },
+    fuel() {
+      this.step = 3;    
+    },
+  },
+  computed: {
+  
+  },
+  methods: {
+    compareResult(obj) { 
+      this.result[obj.step] = obj.value;
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +104,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.grid {
+  display: flex;
+  justify-content: center;
+}
+.grid p {
+  margin: 0 10px;
 }
 </style>
---
+++ src/components/MaterialStep.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="grid">
+        <p>
+          <label for="vulkan_rus">Нержавеющая сталь Вулкан (Россия)</label>
+          <input type="radio" id="vulkan_rus" value="vulkan_rus" v-model="material" />
+        </p>
+        <p v-if="show === 'ugol' || show === 'drova'">
+          <label for="pemza_swe">Вулканическая пемза Heda, Keddy (Швеция)</label>
+          <input type="radio" id="pemza_swe" value="pemza_swe" v-model="material" />
+        </p>
+        <p v-if="show !== 'gaz' || show !== 'dizel'">
+          <label for="hart_ger">Керамическая Hart (Германия)</label>
+          <input type="radio" id="hart_ger" value="hart_ger" v-model="material" />
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MaterialStep',
+    props: {
+        show: String,
+    },
+    data() {
+        return {
+            material: null,
+        }        
+    },
+    watch: {
+        show() {
+            this.material = null;
+        },
+        material() {
+            this.$emit('choose', {step: 'material', value: this.material});
+        }
+    }
+}
+</script>
